03c8eefe625dc14cb043768029833ee28c114c55
Mauro-AGalvezT <antoniogalte@outlook.com>
2021-03-29 22:18:53 -0500

    Layouts de inicio de sesion y registro

---
+++ app/src/main/java/com/galvez/videomeeting/SignInActivity.java
@@ -0,0 +1,19 @@
+package com.galvez.videomeeting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        findViewById(R.id.textSignUp).setOnClickListener(v -> {
+          startActivity(new Intent(getApplicationContext(),SignUpActivity.class));
+        });
+    }
+}
---
+++ app/src/main/java/com/galvez/videomeeting/SignUpActivity.java
@@ -0,0 +1,18 @@
+package com.galvez.videomeeting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        findViewById(R.id.imageBack).setOnClickListener(v -> onBackPressed());
+        findViewById(R.id.textSignIn).setOnClickListener(v -> onBackPressed());
+
+    }
+}
