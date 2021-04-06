b26c125147ba9dbb0e9464a94292565fe56bd131
Mauro-AGalvezT <antoniogalte@outlook.com>
2021-04-06 16:12:09 -0500

    ViewBinding

--- app/src/main/java/com/galvez/videomeeting/Activities/SignInActivity.java
+++ app/src/main/java/com/galvez/videomeeting/Activities/SignInActivity.java
@@ -1,5 +1,6 @@
 package com.galvez.videomeeting.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -13,20 +14,24 @@ import android.widget.Toast;
 import com.galvez.videomeeting.R;
 import com.galvez.videomeeting.Utilities.Constants;
 import com.galvez.videomeeting.Utilities.PreferenceManager;
+import com.galvez.videomeeting.databinding.ActivitySignInBinding;
+import com.galvez.videomeeting.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.material.button.MaterialButton;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SignInActivity extends AppCompatActivity {
 
-    private EditText edtEmail, edtpass;
-    private MaterialButton btnSignIn;
-    private ProgressBar progressBar;
     private PreferenceManager preferenceManager;
+    ActivitySignInBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        View view=binding.getRoot();
+        setContentView(view);
 
         preferenceManager=new PreferenceManager(getApplicationContext());
         if(preferenceManager.getBoolean(Constants.KEY_IS_SIGNED_IN)){
@@ -39,39 +44,34 @@ public class SignInActivity extends AppCompatActivity {
           startActivity(new Intent(getApplicationContext(),SignUpActivity.class));
         });
 
-        edtEmail=findViewById(R.id.inputEmail);
-        edtpass=findViewById(R.id.inputPassword);
-
-        progressBar=findViewById(R.id.signInProgressBar);
-        btnSignIn=findViewById(R.id.buttonSignIn);
-        btnSignIn.setOnClickListener(v -> {
-            if(edtEmail.getText().toString().trim().isEmpty()) {
-                edtEmail.setError("Ingrese su Email");
-                edtEmail.requestFocus();
-            }else if(!Patterns.EMAIL_ADDRESS.matcher(edtEmail.getText().toString()).matches()){
-                edtEmail.setError("Ingrese un Email valido");
-                edtEmail.requestFocus();
-            }else if(edtpass.getText().toString().trim().isEmpty()){
-                edtpass.setError("Ingrese su contraseña");
-                edtpass.requestFocus();
+        binding.buttonSignIn.setOnClickListener(v -> {
+            if(binding.inputEmail.getText().toString().trim().isEmpty()) {
+                binding.inputEmail.setError("Ingrese su Email");
+                binding.inputEmail.requestFocus();
+            }else if(!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()){
+                binding.inputEmail.setError("Ingrese un Email valido");
+                binding.inputEmail.requestFocus();
+            }else if(binding.inputPassword.getText().toString().trim().isEmpty()){
+                binding.inputPassword.setError("Ingrese su contraseña");
+                binding.inputPassword.requestFocus();
             }else{
                 signin();
             }
         });
-
     }
+
     private void signin(){
 
-        btnSignIn.setVisibility(View.INVISIBLE);
-        progressBar.setVisibility(View.VISIBLE);
+        binding.buttonSignIn.setVisibility(View.INVISIBLE);
+        binding.signInProgressBar.setVisibility(View.VISIBLE);
 
         FirebaseFirestore database= FirebaseFirestore.getInstance();
         database.collection(Constants.KEY_COLLECTION_USERS)
-                .whereEqualTo(Constants.KEY_EMAIL,edtEmail.getText().toString())
-                .whereEqualTo(Constants.KEY_PASSWORD,edtpass.getText().toString())
+                .whereEqualTo(Constants.KEY_EMAIL,binding.inputEmail.getText().toString())
+                .whereEqualTo(Constants.KEY_PASSWORD,binding.inputPassword.getText().toString())
                 .get()
                 .addOnCompleteListener(task -> {
-                    if(task.isSuccessful() || task.getResult()!=null || task.getResult().getDocuments().size()>0){
+                    if(task.isSuccessful() && task.getResult()!=null && task.getResult().getDocuments().size()>0){
                         DocumentSnapshot documentSnapshot=task.getResult().getDocuments().get(0);
                         preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN,true);
                         preferenceManager.putString(Constants.KEY_FIRST_NAME,documentSnapshot.getString(Constants.KEY_FIRST_NAME));
@@ -80,11 +80,12 @@ public class SignInActivity extends AppCompatActivity {
                         Intent intent=new Intent(getApplicationContext(),MainActivity.class);
                         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                         startActivity(intent);
-                    }else {
-                        progressBar.setVisibility(View.INVISIBLE);
-                        btnSignIn.setVisibility(View.VISIBLE);
+                    }else  {
+                        binding.signInProgressBar.setVisibility(View.INVISIBLE);
+                        binding.buttonSignIn.setVisibility(View.VISIBLE);
                         Toast.makeText(SignInActivity.this, "No se pudo acceder", Toast.LENGTH_SHORT).show();
                     }
                 });
     }
+
 }
--- app/src/main/java/com/galvez/videomeeting/Activities/SignUpActivity.java
+++ app/src/main/java/com/galvez/videomeeting/Activities/SignUpActivity.java
@@ -1,6 +1,5 @@
 package com.galvez.videomeeting.Activities;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -14,94 +13,85 @@ import android.widget.Toast;
 import com.galvez.videomeeting.R;
 import com.galvez.videomeeting.Utilities.Constants;
 import com.galvez.videomeeting.Utilities.PreferenceManager;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.galvez.videomeeting.databinding.ActivitySignUpBinding;
 import com.google.android.material.button.MaterialButton;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    private EditText inputFirstName,inputLastName,inputEmail,inputPassword,inputConfirmPassword;
-    private MaterialButton buttonSignUp;
-    private ProgressBar signupProgressBar;
     private PreferenceManager preferenceManager;
+    private ActivitySignUpBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        View view=binding.getRoot();
+        setContentView(view);
 
         preferenceManager= new PreferenceManager(getApplicationContext());
+        binding.imageBack.setOnClickListener(v -> onBackPressed());
+        binding.textSignIn.setOnClickListener(v -> onBackPressed());
+        fieldValidation();
 
-
-        findViewById(R.id.imageBack).setOnClickListener(v -> onBackPressed());
-        findViewById(R.id.textSignIn).setOnClickListener(v -> onBackPressed());
-
-        inputFirstName=findViewById(R.id.inputName);
-        inputLastName=findViewById(R.id.inputApellido);
-        inputEmail=findViewById(R.id.inputEmail);
-        inputPassword=findViewById(R.id.inputPassword);
-        inputConfirmPassword=findViewById(R.id.inputConfirmPassword);
-        buttonSignUp=findViewById(R.id.buttonSignUp);
-        signupProgressBar=findViewById(R.id.signUpProgressBar);
-
-        buttonSignUp.setOnClickListener(v -> {
-            if(inputFirstName.getText().toString().trim().isEmpty()){
-                inputFirstName.setError("Campo requerido");
-                inputFirstName.requestFocus();
-            }else if(inputLastName.getText().toString().trim().isEmpty()){
-                inputLastName.setError("Campo requerido");
-                inputLastName.requestFocus();
-            }else if(inputEmail.getText().toString().trim().isEmpty()){
-                inputEmail.setError("Campo requerido");
-                inputEmail.requestFocus();
-            }else if(!Patterns.EMAIL_ADDRESS.matcher(inputEmail.getText().toString()).matches()){
-                inputEmail.setError("Ingrese un correo valido");
-                inputEmail.requestFocus();
-            }else if(inputPassword.getText().toString().trim().isEmpty()){
-                inputPassword.setError("Campo requerido");
-                inputPassword.requestFocus();
-            }else if(inputConfirmPassword.getText().toString().trim().isEmpty()){
-                inputConfirmPassword.setError("Campo requerido");
-                inputConfirmPassword.requestFocus();
-            }else if(!inputPassword.getText().toString().equals(inputConfirmPassword.getText().toString())){
-                inputConfirmPassword.setError("Las contraseñas no coinciden");
-                inputConfirmPassword.requestFocus();
+    }
+    private void fieldValidation(){
+        binding.buttonSignUp.setOnClickListener(v -> {
+            if(binding.inputName.getText().toString().trim().isEmpty()){
+                binding.inputName.setError("Campo requerido");
+                binding.inputName.requestFocus();
+            }else if(binding.inputApellido.getText().toString().trim().isEmpty()){
+                binding.inputApellido.setError("Campo requerido");
+                binding.inputApellido.requestFocus();
+            }else if(binding.inputEmail.getText().toString().trim().isEmpty()){
+                binding.inputEmail.setError("Campo requerido");
+                binding.inputEmail.requestFocus();
+            }else if(!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()){
+                binding.inputEmail.setError("Ingrese un correo valido");
+                binding.inputEmail.requestFocus();
+            }else if(binding.inputPassword.getText().toString().trim().isEmpty()){
+                binding.inputPassword.setError("Campo requerido");
+                binding.inputPassword.requestFocus();
+            }else if(binding.inputConfirmPassword.getText().toString().trim().isEmpty()){
+                binding.inputConfirmPassword.setError("Campo requerido");
+                binding.inputConfirmPassword.requestFocus();
+            }else if(!binding.inputPassword.getText().toString().equals(binding.inputConfirmPassword.getText().toString())){
+                binding.inputConfirmPassword.setError("Las contraseñas no coinciden");
+                binding.inputConfirmPassword.requestFocus();
             }else {
                 signup();
             }
         });
-
     }
 
     private void signup(){
-        buttonSignUp.setVisibility(View.INVISIBLE);
-        signupProgressBar.setVisibility(View.VISIBLE);
+        binding.buttonSignUp.setVisibility(View.INVISIBLE);
+        binding.signUpProgressBar.setVisibility(View.VISIBLE);
 
         FirebaseFirestore database= FirebaseFirestore.getInstance();
         HashMap<String, Object> user = new HashMap<>();
-        user.put(Constants.KEY_FIRST_NAME,inputFirstName.getText().toString());
-        user.put(Constants.KEY_LAST_NAME,inputLastName.getText().toString());
-        user.put(Constants.KEY_EMAIL,inputEmail.getText().toString());
-        user.put(Constants.KEY_PASSWORD,inputPassword.getText().toString());
+        user.put(Constants.KEY_FIRST_NAME,binding.inputName.getText().toString());
+        user.put(Constants.KEY_LAST_NAME,binding.inputApellido.getText().toString());
+        user.put(Constants.KEY_EMAIL,binding.inputEmail.getText().toString());
+        user.put(Constants.KEY_PASSWORD,binding.inputPassword.getText().toString());
 
         database.collection(Constants.KEY_COLLECTION_USERS)
                 .add(user)
                 .addOnSuccessListener(documentReference -> {
 
                     preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN,true);
-                    preferenceManager.putString(Constants.KEY_FIRST_NAME,inputFirstName.getText().toString());
-                    preferenceManager.putString(Constants.KEY_LAST_NAME,inputLastName.getText().toString());
-                    preferenceManager.putString(Constants.KEY_EMAIL,inputEmail.getText().toString());
+                    preferenceManager.putString(Constants.KEY_FIRST_NAME,binding.inputName.getText().toString());
+                    preferenceManager.putString(Constants.KEY_LAST_NAME,binding.inputApellido.getText().toString());
+                    preferenceManager.putString(Constants.KEY_EMAIL,binding.inputEmail.getText().toString());
                     Intent intent=new Intent(getApplicationContext(),MainActivity.class);
                     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(intent);
                 })
                 .addOnFailureListener(e -> {
-                    signupProgressBar.setVisibility(View.INVISIBLE);
-                    buttonSignUp.setVisibility(View.VISIBLE);
+                    binding.signUpProgressBar.setVisibility(View.INVISIBLE);
+                    binding.buttonSignUp.setVisibility(View.VISIBLE);
                     Toast.makeText(SignUpActivity.this, "Error"+e.getMessage(), Toast.LENGTH_SHORT).show();
 
                 });
