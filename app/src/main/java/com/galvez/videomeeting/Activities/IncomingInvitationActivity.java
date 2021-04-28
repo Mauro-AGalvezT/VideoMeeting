716c5e3f9458d6923d3114c8e3935257285320ed
Mauro-AGalvezT <antoniogalte@outlook.com>
2021-04-27 23:32:21 -0500

    aceptar,rechazar y cancelar invitacion

--- app/src/main/java/com/galvez/videomeeting/Activities/IncomingInvitationActivity.java
+++ app/src/main/java/com/galvez/videomeeting/Activities/IncomingInvitationActivity.java
@@ -1,13 +1,25 @@
 package com.galvez.videomeeting.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.galvez.videomeeting.Network.ApiClient;
+import com.galvez.videomeeting.Network.ApiService;
 import com.galvez.videomeeting.R;
 import com.galvez.videomeeting.Utilities.Constants;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class IncomingInvitationActivity extends AppCompatActivity {
 
@@ -39,5 +51,66 @@ public class IncomingInvitationActivity extends AppCompatActivity {
                 getIntent().getStringExtra(Constants.KEY_LAST_NAME)
         ));
         textEmail.setText(getIntent().getStringExtra(Constants.KEY_EMAIL));
+
+        ImageView imageAcceptedInvitation= findViewById(R.id.imageAcceptedInvitation);
+        imageAcceptedInvitation.setOnClickListener(v -> sendInvitationResponse(
+                Constants.REMOTE_MSG_INVITATION_ACCEPTED,
+                getIntent().getStringExtra(Constants.REMOTE_MSG_INVITER_TOKEN)
+        ));
+
+        ImageView imageRejectedInvitation = findViewById(R.id.imageRejectInvitation);
+        imageRejectedInvitation.setOnClickListener(v -> sendInvitationResponse(
+                Constants.REMOTE_MSG_INVITATION_REJECTED,
+                getIntent().getStringExtra(Constants.REMOTE_MSG_INVITER_TOKEN)
+        ));
+    }
+
+    private void sendInvitationResponse(String type, String receiverToken){
+
+        try {
+            JSONArray tokens= new JSONArray();
+            tokens.put(receiverToken);
+
+            JSONObject body = new JSONObject();
+            JSONObject data = new JSONObject();
+
+            data.put(Constants.REMOTE_MSG_TYPE,Constants.REMOTE_MSG_INVITATION_RESPONSE);
+            data.put(Constants.REMOTE_MSG_INVITATION_RESPONSE,type);
+
+            body.put(Constants.REMOTE_MSG_DATA,data);
+            body.put(Constants.REMOTE_MSG_REGISTRATION_IDS,tokens);
+
+            sendRemoteMessage(body.toString(),type);
+
+        }catch (Exception exception){
+            Toast.makeText(this, exception.getMessage(), Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+    private void sendRemoteMessage(String remoteMessageBody, String type){
+        ApiClient.getClient().create(ApiService.class).sendRemoteMessage(
+                Constants.getRemoteMessageHeards(),remoteMessageBody
+        ).enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(@NonNull Call<String> call, @NonNull Response<String> response) {
+                if(response.isSuccessful()){
+                    if (type.equals(Constants.REMOTE_MSG_INVITATION_ACCEPTED)){
+                        Toast.makeText(IncomingInvitationActivity.this, "Invitacion aceptada", Toast.LENGTH_SHORT).show();
+                    }else {
+                        Toast.makeText(IncomingInvitationActivity.this, "Invitacion rechazada", Toast.LENGTH_SHORT).show();
+                    }
+                }else {
+                    Toast.makeText(IncomingInvitationActivity.this, response.message(), Toast.LENGTH_SHORT).show();
+                }
+                finish();
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<String> call, @NonNull Throwable t) {
+                Toast.makeText(IncomingInvitationActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/galvez/videomeeting/Firebase/MessagingService.java
+++ app/src/main/java/com/galvez/videomeeting/Firebase/MessagingService.java
@@ -2,6 +2,7 @@ package com.galvez.videomeeting.Firebase;
 import android.content.Intent;
 
 import androidx.annotation.NonNull;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.galvez.videomeeting.Activities.IncomingInvitationActivity;
 import com.galvez.videomeeting.Utilities.Constants;
@@ -43,8 +44,25 @@ public class MessagingService extends FirebaseMessagingService {
                         remoteMessage.getData()
                                 .get(Constants.KEY_EMAIL)
                 );
+                intent.putExtra(
+                        Constants.REMOTE_MSG_INVITER_TOKEN,
+                        remoteMessage.getData()
+                                .get(Constants.REMOTE_MSG_INVITER_TOKEN)
+                );
+                intent.putExtra(
+                        Constants.REMOTE_MSG_INVITER_TOKEN,
+                        remoteMessage.getData()
+                                .get(Constants.REMOTE_MSG_INVITER_TOKEN)
+                );
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
+            }else if (type.equals(Constants.REMOTE_MSG_INVITATION_RESPONSE)){
+                Intent intent=new Intent(Constants.REMOTE_MSG_INVITATION_RESPONSE);
+                intent.putExtra(
+                        Constants.REMOTE_MSG_INVITATION_RESPONSE,
+                        remoteMessage.getData().get(Constants.REMOTE_MSG_INVITATION_RESPONSE)
+                );
+                LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(intent);
             }
         }
 
